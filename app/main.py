262eb933918734ed6a87c2eef24689877e70b7ec
Jobi S S <jobisjobi1234@gmail.com>
2024-04-20 20:08:12 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],  
+    allow_headers=["https://sample-firebase-project-883bd.web.app/"],  
 )
 # @app.get('/')
 # def hello_world():
